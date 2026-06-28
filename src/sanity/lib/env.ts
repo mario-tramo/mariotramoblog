26d21826ce7d5cb7aac785ba75345d0965da9811
fvastu <fra.vasturzo@gmail.com>
2026-06-28 17:56:47 +0200

    refactor: update sanity env and fetch with new config

--- src/sanity/lib/env.ts
+++ src/sanity/lib/env.ts
@@ -14,5 +14,7 @@ export const dataset = requireEnv(
 	process.env.NEXT_PUBLIC_SANITY_DATASET,
 )
 
-export const apiVersion =
-	process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-12-01'
+export const apiVersion = requireEnv(
+	'NEXT_PUBLIC_SANITY_API_VERSION',
+	process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+)
